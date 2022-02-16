8f3f4d0eeb369535382a4d4abd2d34213d4a055c
1000marin <leexjin@gmail.com>
2022-02-16 16:32:52 +0900

    배열

--- src/main/java/Class1216.java
+++ src/main/java/Class1216.java
@@ -11,12 +11,17 @@ public class Class1216 {
         array[0][3] = 4;
         array[0][4] = 5;
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                System.out.print(array[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
+        int a = 0;
+        while (a < 5) {
+            a++;
+            System.out.println(a);
+        }
     }
 }
